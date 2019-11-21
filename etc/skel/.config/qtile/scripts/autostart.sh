cff11a3b30eb4166ca09aacdcf6b9287d7a70c49
Erik Dubois <erik.dubois@gmail.com>
2019-11-21 06:57:40 +0100

    compton to picom

--- etc/skel/.config/qtile/scripts/autostart.sh
+++ etc/skel/.config/qtile/scripts/autostart.sh
@@ -32,7 +32,7 @@ run pamac-tray &
 run xfce4-power-manager &
 numlockx on &
 blueberry-tray &
-compton --config $HOME/.config/qtile/scripts/compton.conf &
+picom --config $HOME/.config/qtile/scripts/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 

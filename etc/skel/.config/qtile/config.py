19dd42ea3c85b747805e09b84f66497194a155ad
Erik Dubois <erik.dubois@gmail.com>
2020-01-14 12:21:52 +0100

    new assign to ws

--- etc/skel/.config/qtile/config.py
+++ etc/skel/.config/qtile/config.py
@@ -268,8 +268,9 @@ group_names = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0",]
 # FOR AZERTY KEYBOARDS
 #group_names = ["ampersand", "eacute", "quotedbl", "apostrophe", "parenleft", "section", "egrave", "exclam", "ccedilla", "agrave",]
 
-group_labels = ["", "", "", "", "", "", "", "", "", "",]
-#group_labels = ["Web", "Edit", "Ink", "Gimp", "Meld", "Vlc", "VB", "Thunar", "Mail", "Music",]
+#group_labels = ["1 ", "2 ", "3 ", "4 ", "5 ", "6 ", "7 ", "8 ", "9 ", "0",]
+group_labels = ["", "", "", "", "", "", "", "", "", "",]
+#group_labels = ["Web", "Edit/chat", "Image", "Gimp", "Meld", "Video", "Vb", "Files", "Mail", "Music",]
 
 group_layouts = ["monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall", "monadtall",]
 #group_layouts = ["monadtall", "matrix", "monadtall", "bsp", "monadtall", "matrix", "monadtall", "bsp", "monadtall", "monadtall",]
@@ -556,30 +557,37 @@ dgroups_app_rules = []
 # ASSIGN APPLICATIONS TO A SPECIFIC GROUPNAME
 # BEGIN
 
-# @hook.subscribe.client_new
-# def assign_app_group(client):
-#     d = {}
-#     #########################################################
-#     ################ assgin apps to groups ##################
-#     #########################################################
-#     d["1"] = ["Navigator", "vivaldi-stable", "chromium", "brave-browser", "google-chrome" ]
-#     d["2"] = ["subl3", "atom", "geany", "brackets", "code-oss", "code" ]
-#     d["3"] = ["inkscape", "nomacs", ]
-#     d["4"] = ["gimp", ]
-#     d["5"] = ["org.gnome.meld", ]
-#     d["6"] = ["vlc", ]
-#     d["7"] = ["VirtualBox Manager", "VirtualBox Machine", "Vmplayer", ]
-#     d["8"] = ["thunar", "nemo", "caja", "org.gnome.Nautilus", "pcmanfm", "pcmanfm-qt" ]
-#     d["9"] = ["evolution", "geary" ]
-#     d["0"] = ["spotify", "pragha", "clementine", "deadbeef", "audacious" ]
-#     ##########################################################
-#     wm_class = client.window.get_wm_class()[0]
-#
-#     for i in range(len(d)):
-#         if wm_class in list(d.values())[i]:
-#             group = list(d.keys())[i]
-#             client.togroup(group)
-#             client.group.cmd_toscreen()
+@hook.subscribe.client_new
+def assign_app_group(client):
+    d = {}
+    #########################################################
+    ################ assgin apps to groups ##################
+    #########################################################
+    d["1"] = ["Navigator", "Firefox", "Vivaldi-stable", "Vivaldi-snapshot", "Chromium", "Google-chrome", "Brave", "Brave-browser",
+              "navigator", "firefox", "vivaldi-stable", "vivaldi-snapshot", "chromium", "google-chrome", "brave", "brave-browser", ]
+    d["2"] = [ "Atom", "Subl3", "Geany", "Brackets", "Code-oss", "Code", "TelegramDesktop", "Discord",
+               "atom", "subl3", "geany", "brackets", "code-oss", "code", "telegramDesktop", "discord", ]
+    d["3"] = ["Inkscape", "Nomacs", "Ristretto", "Nitrogen", "Feh",
+              "inkscape", "nomacs", "ristretto", "nitrogen", "feh", ]
+    d["4"] = ["Gimp", "gimp" ]
+    d["5"] = ["Meld", "meld", "org.gnome.meld" "org.gnome.Meld" ]
+    d["6"] = ["Vlc","vlc", "Mpv", "mpv" ]
+    d["7"] = ["VirtualBox Manager", "VirtualBox Machine", "Vmplayer",
+              "virtualbox manager", "virtualbox machine", "vmplayer", ]
+    d["8"] = ["Thunar", "Nemo", "Caja", "Nautilus", "org.gnome.Nautilus", "Pcmanfm", "Pcmanfm-qt",
+              "thunar", "nemo", "caja", "nautilus", "org.gnome.nautilus", "pcmanfm", "pcmanfm-qt", ]
+    d["9"] = ["Evolution", "Geary", "Mail", "Thunderbird",
+              "evolution", "geary", "mail", "thunderbird" ]
+    d["0"] = ["Spotify", "Pragha", "Clementine", "Deadbeef", "Audacious",
+              "spotify", "pragha", "clementine", "deadbeef", "audacious" ]
+    ##########################################################
+    wm_class = client.window.get_wm_class()[0]
+
+    for i in range(len(d)):
+        if wm_class in list(d.values())[i]:
+            group = list(d.keys())[i]
+            client.togroup(group)
+            client.group.cmd_toscreen()
 
 # END
 # ASSIGN APPLICATIONS TO A SPECIFIC GROUPNAME

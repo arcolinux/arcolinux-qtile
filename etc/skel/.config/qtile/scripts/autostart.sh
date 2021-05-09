5aca54dde8b285d812fa13da95e1eb17ac06c88f
Erik Dubois <erik.dubois@gmail.com>
2021-05-09 09:25:42 +0200

    autorandr in

--- etc/skel/.config/qtile/scripts/autostart.sh
+++ etc/skel/.config/qtile/scripts/autostart.sh
@@ -16,6 +16,7 @@ function run {
 #xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
 #xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
 #xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+#autorandr horizontal
 
 #change your keyboard if you need it
 #setxkbmap -layout be

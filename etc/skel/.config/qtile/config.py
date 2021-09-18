399e2dd1654cf6ccf95be6b9c74c962b5dff81b1
Erik Dubois <erik.dubois@gmail.com>
2021-09-18 07:10:48 +0200

    import Match

--- etc/skel/.config/qtile/config.py
+++ etc/skel/.config/qtile/config.py
@@ -29,9 +29,10 @@ import os
 import re
 import socket
 import subprocess
-from libqtile.config import Drag, Key, Screen, Group, Drag, Click, Rule
-from libqtile.command import lazy
+from typing import List  # noqa: F401
 from libqtile import layout, bar, widget, hook
+from libqtile.config import Click, Drag, Group, Key, Match, Screen, Rule
+from libqtile.command import lazy
 from libqtile.widget import Spacer
 #import arcobattery
 
@@ -536,12 +537,12 @@ cursor_warp = False
 floating_layout = layout.Floating(float_rules=[
     # Run the utility of `xprop` to see the wm class and name of an X client.
     *layout.Floating.default_float_rules, 
-    Match(wm_class='confirmreset'),  # gitk 
-    Match(wm_class='makebranch'),  # gitk 
-    Match(wm_class='maketag'),  # gitk 
-    Match(wm_class='ssh-askpass'),  # ssh-askpass 
-    Match(title='branchdialog'),  # gitk 
-    Match(title='pinentry'),  # GPG key password entry 
+    Match(wm_class='confirmreset'),  # gitk
+    Match(wm_class='makebranch'),  # gitk
+    Match(wm_class='maketag'),  # gitk
+    Match(wm_class='ssh-askpass'),  # ssh-askpass
+    Match(title='branchdialog'),  # gitk
+    Match(title='pinentry'),  # GPG key password entry
     Match(wm_class='Arcolinux-welcome-app.py'),
     Match(wm_class='Arcolinux-tweak-tool.py'),
     Match(wm_class='Arcolinux-calamares-tool.py'),

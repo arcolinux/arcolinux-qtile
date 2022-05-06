e9817603f5c7e1eda275642171c7f9a8f8b63cf8
arcolinuxz <arcolinuxinfo@gmail.com>
2022-05-06 16:15:39 +0200

    arch logo

--- etc/skel/.config/qtile/config-qwerty.py
+++ etc/skel/.config/qtile/config-qwerty.py
@@ -579,7 +579,7 @@ floating_layout = layout.Floating(float_rules=[
     Match(wm_class='Arandr'),
     Match(wm_class='feh'),
     Match(wm_class='Galculator'),
-    Match(wm_class='arcolinux-logout'),
+    Match(wm_class='archlinux-logout'),
     Match(wm_class='xfce4-terminal'),
 
 ],  fullscreen_border_width = 0, border_width = 0)

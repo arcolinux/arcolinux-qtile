24d91824c166fa0756d1cce04019cbeee79f20e2
Erik Dubois <erik.dubois@gmail.com>
2024-12-07 06:31:11 +0100

    arco

--- etc/skel/.config/qtile/scripts/autostart.sh
+++ etc/skel/.config/qtile/scripts/autostart.sh
@@ -26,7 +26,7 @@ run dex $HOME/.config/autostart/arcolinux-welcome-app.desktop &
 
 #Some ways to set your wallpaper besides variety or nitrogen
 feh --bg-fill /usr/share/backgrounds/archlinux/arch-wallpaper.jpg &
-feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
+feh --bg-fill /usr/share/backgrounds/arco/arco-wallpaper.jpg &
 #wallpaper for other Arch based systems
 #feh --bg-fill /usr/share/archlinux-tweak-tool/data/wallpaper/wallpaper.png &
 #start the conky to learn the shortcuts

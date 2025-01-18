5976ec5cf7e30fc4f69a15a9949691f5dc0ad7d6
arcolinuxz <arcolinuxinfo@gmail.com>
2025-01-18 10:01:10 +0100

    coming from default qtile config

--- etc/skel/.config/qtile/config-azerty.py
+++ etc/skel/.config/qtile/config-azerty.py
@@ -592,6 +592,7 @@ floating_types = ["notification", "toolbar", "splash", "dialog"]
 
 follow_mouse_focus = True
 bring_front_click = False
+floats_kept_above = True
 cursor_warp = False
 floating_layout = layout.Floating(float_rules=[
     # Run the utility of `xprop` to see the wm class and name of an X client.
@@ -621,6 +622,26 @@ floating_layout = layout.Floating(float_rules=[
 ],  fullscreen_border_width=0, border_width=0)
 auto_fullscreen = True
 
-focus_on_window_activation = "focus"  # or smart
+focus_on_window_activation = "focus" # or smart
+reconfigure_screens = True
 
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
+
+# When using the Wayland backend, this can be used to configure input devices.
+wl_input_rules = None
+
+# xcursor theme (string or None) and size (integer) for Wayland backend
+wl_xcursor_theme = None
+wl_xcursor_size = 24
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
 wmname = "LG3D"
